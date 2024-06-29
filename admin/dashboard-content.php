c3c45b614c1418ca6cf4f607eabc07d9994c9f53
aisyahwlandr <aisyahwlandr@gmail.com>
2024-06-29 16:20:49 +0700

    change ui margin dashboard content

--- admin/dashboard-content.php
+++ admin/dashboard-content.php
@@ -1,4 +1,4 @@
-<div data-aos="zoom-in-up" data-aos-duration="1000" class="container my-5" width="900" height="380">
+<div data-aos="zoom-in-up" data-aos-duration="1000" class="container " width="900" height="380" style="margin-top: 15vh; margin-bottom: 15vh;">
     <div class="p-5 text-center rounded-3" style="background-color: #38419D;">
         <h1 class="text-white">Selamat Datang di Dashboard Admin<br>Dapur Sehat</h1>
         <p class="lead" style="color: #DADDFC;">Lakukan Manajemen Pekerjaan dengan Bijak dan Tanggung Jawab</p>
